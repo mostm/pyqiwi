6ff4eadd9a189d06e4b8654c555c51a950183a3a
Levent Duivel <mostm@endcape.ru>
2018-05-07 08:59:17 +0500

    Fix: pyqiwi.detect_mobile возвращал всегда None

--- pyqiwi/apihelper.py
+++ pyqiwi/apihelper.py
@@ -195,7 +195,7 @@ def identification(token, wallet, birth_date, first_name, middle_name, last_name
 def detect(phone):
     result_json = requests.post('https://qiwi.com/mobile/detect.action', data={"phone": phone})
     result_json = result_json.json()
-    if result_json.get('code', {}).get('value') == 0:
+    if result_json.get('code', {}).get('value') == '0':
         return result_json.get('message')
     else:
         return None
